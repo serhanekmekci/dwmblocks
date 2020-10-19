0018b85864fcd2ce569f5603eaa942b70ac70324
Serhan Ekmekçi <serhanekmekci@protonmail.com>
2020-10-19 23:21:40 +0300

    blocks

--- blocks.def.h
+++ blocks.def.h
@@ -1,8 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "date '+%b %d (%a) %I:%M%p'", 1, 0},
-	{" ", "bar_kbdlayout", 0, 1},
+	{" ", "bar_kbdlayout", 0, 2},
+	{"", "bar_date", 1, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
