03f9c235b8b0cbc9842c97f6159afef00e2917d3
Serhan Ekmekçi <serhanekmekci@protonmail.com>
2020-10-20 17:09:55 +0300

    blocks

--- blocks.def.h
+++ blocks.def.h
@@ -1,10 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "bar_windows", 60, 7},
-	{"", "bar_os_x", 60, 6},
-	{"", "bar_artix", 60, 5},
-	{"", "bar_wifi", 1, 4},
+	{"", "bar_windows", 60, 8},
+	{"", "bar_os_x", 60, 7},
+	{"", "bar_artix", 60, 6},
+	{"", "bar_wifi", 1, 5},
+	{"", "volume", 0 4},
 	{" ", "bar_kbdlayout", 0, 3},
 	{" " , "bar_date", 1, 2},
 	{" " , "bar_time", 1, 1},
