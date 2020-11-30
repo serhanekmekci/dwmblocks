f842cb9fd0f97069c2bc33c6f6f8e6c5d4648ad2
Serhan Ekmekçi <serhan@ekmekci.me>
2020-11-30 21:44:28 +0300

    changed intervals

--- blocks.def.h
+++ blocks.def.h
@@ -1,19 +1,19 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "bar_news", 1, 12},
-	{"", "bar_mailbox", 1, 11},
-	{"", "bar_checkwg", 1, 13},
-	{"", "bar_windows", 60, 8},
-	{"", "bar_os_x", 60, 7},
-	{"", "bar_artix", 60, 6},
+	{"", "bar_news", 600, 12},
+	{"", "bar_mailbox", 300, 11},
+	{"", "bar_checkwg", 5, 13},
+	{"", "bar_windows", 300, 8},
+	{"", "bar_os_x", 300, 7},
+	{"", "bar_artix", 300, 6},
 	{"", "forecast", 10800, 10},
-	{"", "bar_wifi", 5, 5},
+	{"", "bar_wifi", 10, 5},
 	{"", "bar_volume", 0, 4},
 	{" ", "bar_kbdlayout", 0, 3},
 	{"", "bar_bluetooth", 0, 9},
 	{" " , "bar_date", 0, 2},
-	{" " , "bar_time", 1, 1},
+	{" " , "bar_time", 10, 1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
